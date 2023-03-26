e9c23dd3dd8cd6e53b7923753c77500256dea462
reddit-tthompson5 <128263236+reddit-tthompson5@users.noreply.github.com>
2023-03-25 23:49:17 -0600

    Add files via upload
    Re-wrote roster header file to accept version control numbers in the case that not the entire roster is wanted for a summon simulation. Default values were assigned to the functions, so they still work when the functions are called without parameters.

--- FEH_roster_header.R
+++ FEH_roster_header.R
@@ -1,70 +1,63 @@
+rm(list=ls())
 roster = read.csv("FEH_Full_Roster.csv")
 
-special_ver_cutoff = 4.08 #version cutoff for 4-star special heroes
-
-#finding the indexes of the different color types of 4-star special heroes
-
-red_4s_s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Red" & roster$version <= special_ver_cutoff)
-
-blue_4s_s = which(((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Blue" & roster$version <= special_ver_cutoff) |
-		roster$name == "Ephraim" & roster$type == "Duo")
-
-green_4s_s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Green" & roster$version <= special_ver_cutoff)
-
-cless_4s_s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Cless" & roster$version <= special_ver_cutoff)
-
-#finding the indexes of the different regular 5-star heroes that are NOT
-#4-star special heroes
-
-reg_red_5s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Red" & roster$version > special_ver_cutoff)
-
-reg_blue_5s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Blue" & roster$version > special_ver_cutoff)
-
-reg_green_5s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Green" & roster$version > special_ver_cutoff)
-
-reg_cless_5s = which((roster$type == "Five-star Regular" |
-		roster$type == "Brave" | roster$type == "Ascendant") & 
-		roster$color == "Cless" & roster$version > special_ver_cutoff)
-
-#finding the indexes of the 3/4-star heroes
-
-red_demote = which (roster$type == "Demote Regular" &
-	roster$color == "Red")
-
-blue_demote = which (roster$type == "Demote Regular" &
-	roster$color == "Blue")
-
-green_demote = which (roster$type == "Demote Regular" &
-	roster$color == "Green")
-
-cless_demote = which (roster$type == "Demote Regular" &
-	roster$color == "Cless")
-
-reg_five_stars = function()
+#reg_five_stars is a fuction that counts all of the regular 5-star 
+#(pitybreaker) heroes by color and returns them. "ver_cutoff" is the max
+#version of heroes to include. "spc_ver_cutoff" indicates the highest ver
+#number of 4-star special heroes, that aren't to be included in the count.
+reg_five_stars = function(ver_cutoff=max(roster$version), spc_ver_cutoff=4.08)
 {
-	red = length(reg_red_5s)
-	blue = length(reg_blue_5s)
-	green = length(reg_green_5s)
-	cless = length(reg_cless_5s)
-	return(c(red, blue, green, cless))
+	reg_red_5s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Red" & roster$version <= ver_cutoff & 
+		roster$version > spc_ver_cutoff)
+		#find indicies of regular red 5-star heroes
+	
+	reg_blue_5s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Blue" & roster$version <= ver_cutoff & 
+		roster$version > spc_ver_cutoff)
+		#find indicies of regular blue 5-star heroes
+
+	reg_green_5s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Green" & roster$version <= ver_cutoff & 
+		roster$version > spc_ver_cutoff)
+		#find indicies of regular green 5-star heroes
+
+	reg_cless_5s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Cless" & roster$version <= ver_cutoff &
+		roster$version > spc_ver_cutoff)
+		#find indicies of regular cless 5-star heroes
+
+	red = length(reg_red_5s) #calc number of red 5-stars
+	blue = length(reg_blue_5s) #calc number of blue 5-stars
+	green = length(reg_green_5s) #calc number of green 5-stars
+	cless = length(reg_cless_5s) #calc number of cless 5-stars
+	return(c(red, blue, green, cless)) #return values
 }
 
-four_star_specials = function()
+#similar to "reg_five_stars" function but counts 4-star special heroes instead
+four_star_specials = function(spc_ver_cutoff=4.08)
 {
+	red_4s_s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Red" & roster$version <= spc_ver_cutoff)
+	
+	blue_4s_s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant" |
+		(roster$name == "Ephraim" & roster$name == "Dynastic Duo")) & 
+		roster$color == "Blue" & roster$version <= spc_ver_cutoff)
+
+	green_4s_s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Green" & roster$version <= spc_ver_cutoff)
+
+	cless_4s_s = which((roster$type == "Five-star Regular" |
+		roster$type == "Brave" | roster$type == "Ascendant") & 
+		roster$color == "Cless" & roster$version <= spc_ver_cutoff)
+
 	red = length(red_4s_s)
 	blue = length(blue_4s_s)
 	green = length(green_4s_s)
@@ -72,8 +65,21 @@ four_star_specials = function()
 	return(c(red, blue, green, cless))
 }
 
-demotes = function()
+#similar to "reg_five_stars" function but counts demote (3/4-star) heroes
+demotes = function(ver_cutoff=max(roster$version))
 {
+	red_demote = which (roster$type == "Demote Regular" &
+		roster$color == "Red" & roster$version <= ver_cutoff)
+
+	blue_demote = which (roster$type == "Demote Regular" &
+		roster$color == "Blue" & roster$version <= ver_cutoff)
+
+	green_demote = which (roster$type == "Demote Regular" &
+		roster$color == "Green" & roster$version <= ver_cutoff)
+
+	cless_demote = which (roster$type == "Demote Regular" &
+		roster$color == "Cless" & roster$version <= ver_cutoff)
+	
 	red = length(red_demote)
 	blue = length(blue_demote)
 	green = length(green_demote)
